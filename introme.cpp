922dd2820822eed8cbfe5efd47e8fe15f56e89f9
John Cashman <jbunny63@outlook.com>
2025-05-21 16:42:58 -0500

    Create introme.cpp
    tiny.. pipe..

---
+++ introme.cpp
@@ -0,0 +1,8 @@
+//introme.cpp  CopyMIT (c) and so on 2025 John Cashman.
+//Okay, I see what I did there.
+//This is painful. I must remember...
+//Otherwise a cat-tastic day in Paradiso!
+int main()==0; 
+{int happyness==1;
+ return 1;
+}
